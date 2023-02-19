dfc2e6120f3d4725d1100dacd0b59a8f24da539c
f3rn4nd0xyz <contasuch123@gmail.com>
2023-02-19 03:30:56 -0300

    frontend and data gathering from stripe api done

---
+++ src/components/SingInButton/index.tsx
@@ -0,0 +1,21 @@
+import { FiX } from 'react-icons/fi'
+import { FaGithub } from 'react-icons/fa'
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? (
+<button type="button" className={styles.signInButton}>
+            <FaGithub color='#04d361'/>
+            f3rn4nd0xyz
+            <FiX color='#737380' className={styles.closeIcon}/>
+        </button>
+    ) : (
+        <button type="button" className={styles.signInButton}>
+            <FaGithub color='#eba417'/>
+            Sign in with Github 
+        </button>
+
+    );
+}
